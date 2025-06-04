39d7fea666ef23f687a245e91532bdb14c6860ab
chengq220 <chengqiu220@gmail.com>
2025-06-03 21:38:19 -0400

    finish popup

--- src/components/List.js
+++ src/components/List.js
@@ -1,6 +1,12 @@
+import { useState } from "react";
+import Popup from "./PopUp";
+
 function Entry({item}){
+    const [popUp, setPopUp] = useState(false);
+
     return(
         <div
+            onClick = {() => setPopUp(true)}
             className="flex flex-row items-center h-24 rounded-lg overflow-hidden hover:bg-gray-200 cursor-pointer">
             <div className = "basis-[30%] flex justify-center">
                 <img className="object-scale-down h-20 w-20 rounded-xl border" src={item["thumbnail"]} alt="thumbnail"/>
@@ -9,6 +15,7 @@ function Entry({item}){
                 <h1 className="font-semibold">{item["track_name"]}</h1>
                 <p className="text-gray-400">{item["artists"]}</p>
             </div>
+           {popUp && <Popup data = {item} closePopUp= {() => setPopUp(false)} />}
         </div>
     );
 }
--- src/components/PopUp.js
+++ src/components/PopUp.js
@@ -1,41 +1,44 @@
-import { useState } from 'react';
+import { useState, useEffect } from 'react';
 
-const PopUp = ({ openPopUp, closePopUp }) => {
-
-    const [pl, setPL] = useState('')
-
-    const handleChange = (event) => {
-        console.log(pl)
-        setPL(event.target.value)
-    }
+const PopUp = ({ data, closePopUp }) => {
 
     const handlelosePopUp = (e) => {
+        e.stopPropagation();
         if (e.target.id === 'ModelContainer') {
             closePopUp();
         }
     }
 
-    if (openPopUp !== true) return null
+    useEffect(() => {
+        const handleEsc = (event) => {
+            if (event.key === 'Escape') {
+                closePopUp();
+                console.log('Close');
+            }
+        };
+        window.addEventListener('keydown', handleEsc);
+
+        return () => {
+            window.removeEventListener('keydown', handleEsc);
+        };
+    }, []);
 
     return (
-    <div
-        id='ModelContainer'
-        onClick={handlelosePopUp}
-        className='fixed inset-0 bg-black flex justify-center items-center bg-opacity-20 backdrop-blur-sm'>
-        <div className='p-2 bg-white w-10/12 md:w-1/2 lg:1/3 shadow-inner border-e-emerald-600 rounded-lg py-5'>
-            <div className='w-full p-3 justify-center items-center flex flex-row'>
-                <h2 className='font-semibold py-3 text-center text-xl'>Playlist Name:</h2>
-                <input 
-                    type="text" 
-                    name="preference" 
-                    id="pref" 
-                    onChange={handleChange} 
-                    required 
-                    className="block w-full rounded-md bg-white px-3 py-1.5 text-base text-gray-900 outline-1 -outline-offset-1 outline-gray-300 placeholder:text-gray-400 focus:outline-2 focus:-outline-offset-2 focus:outline-indigo-600 sm:text-sm/6 "  />
-                <button onClick={}>Submit</button>
+        <div
+            id='ModelContainer'
+            onClick={handlelosePopUp}
+            className='fixed inset-0 bg-black rounded-xl flex justify-center items-center bg-opacity-5 backdrop-blur-sm'>
+            <div className='p-2 bg-white w-4/12 md:w-2/5 lg:1/5 md:h-1/2 shadow-inner border-e-emerald-600 rounded-lg py-5'>
+                <div className='w-1/2 p-3 justify-center items-center flex flex-col'>
+                    <div>
+                        <img className="rounded-xl" src={data["thumbnail"]} alt="thumbnail"/>
+                    </div>
+                    <h2 className="text-lg font-bold">{data['track_name']}</h2>
+                    <p>{data['artists']}</p>
+                    <p className="text-sm text-gray-500">{data['track_genre']}</p>
+                </div>
             </div>
         </div>
-    </div>
     )
 }
 
